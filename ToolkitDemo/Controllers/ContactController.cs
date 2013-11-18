c961043bd6e6c3a36d3bef728c9634c37a2a2023
Tsingbo-Kooboo <qingbo@kooboo.com>
2013-11-18 19:44:08 +0800

    Cms_Data files

---
+++ ToolkitDemo/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using Kooboo.CMS.Common;
+using Kooboo.CMS.Sites.Globalization;
+using Kooboo.CMS.Sites.Models;
+using Kooboo.CMS.Toolkit;
+using Kooboo.CMS.Toolkit.Filters;
+using ToolkitDemo.Models;
+
+namespace ToolkitDemo.Controllers
+{
+    public class ContactController : Controller
+    {
+        [CMSPageExecutionFilter]
+        [HttpPost]
+        public ActionResult Submit(Contact contact)
+        {
+            AntiForgery.Validate();
+            var data = new JsonResultData(ModelState);
+            data.RunWithTry((resultData) =>
+            {
+                var body = ControllerContext.RenderView("Contact.EmailTemplate", contact);
+                var smtp = Site.Current.Smtp;
+                if (null != smtp && !String.IsNullOrEmpty(smtp.From) && null != smtp.To && smtp.To.Any())
+                {
+                    foreach (var mail in smtp.To.Select(to => new MailMessage(smtp.From, to, contact.Subject, body)))
+                    {
+                        EmailUtility.Send(mail);
+                    }
+                    resultData.AddMessage("Send email to us success!We will reply you ASAP".RawLabel("SendEmailSuccess").ToString());
+                    resultData.ReloadPage = true;
+                }
+                else
+                {
+                    resultData.AddErrorMessage("Smtp was not configured in Site setting".RawLabel("SmtpRequired").ToString());
+                }
+            });
+            return Json(data);
+        }
+    }
+}
---
+++ WebSite/Cms_Data/Sites/ToolkitDemo/Scripts/app.js
@@ -0,0 +1,99 @@
+﻿$(function () {
+    //Fancybox
+    $('#login-link').fancybox({
+        showCloseButton: false,
+        overlayColor: '#000',
+        opacity: 0.3,
+        autoDimensions: false,
+        height: 260
+    });
+
+    $('.ajaxForm').each(function () {
+        var form = $(this);
+        form.ajaxForm({
+            sync: true,
+            beforeSerialize: function ($form, options) {
+            },
+            beforeSend: function () {
+                var form = $(this);
+                form.find("[type=submit]").addClass("disabled").attr("disabled", true);
+            },
+            beforeSubmit: function (arr, $form, options) {
+            },
+            success: function (responseData, statusText, xhr, $form) {
+                form.find("[type=submit]").removeClass("disabled").removeAttr("disabled");
+                if (!responseData.Success) {
+                    var validator = form.validate();
+                    //                            var errors = [];
+                    for (var i = 0; i < responseData.FieldErrors.length; i++) {
+                        var obj = {};
+                        var fieldName = responseData.FieldErrors[i].FieldName;
+                        if (fieldName == "") {
+                            alert(responseData.FieldErrors[i].ErrorMessage);
+                        }
+                        obj[fieldName] = responseData.FieldErrors[i].ErrorMessage;
+                        validator.showErrors(obj);
+                    }
+                } else {
+                    if (responseData.RedirectUrl != null) {
+                        location.href = responseData.RedirectUrl;
+                    } else {
+                        location.reload();
+                    }
+
+                }
+            },
+            error: function () {
+                var form = $(this);
+                form.find("[type=submit]").removeClass('disabled').removeAttr('disabled');
+            }
+        });
+    });
+});
+//#region Comment Page
+function AddComment(response) {
+    var responseData = $.parseJSON(response.responseText);
+    if (responseData.Success) {
+        var $model = $(responseData.Model);
+        if ($(".item:last").length) {
+            //append added comment to comment list
+            $model.insertAfter(".item:last");
+        } else {
+            $model.insertAfter("#comments h6.title:first");
+        }
+        document.getElementById("comment-form").reset();
+        //InlineEditor
+        if (window.yardi) {
+            window.yardi.initInlineEditing();
+        }
+    } else {
+        var errorMsg = responseData.Messages.join("<br/>");
+        alert(errorMsg);
+    }
+}
+
+function DeleteComment(response) {
+    var json = $.parseJSON(response.responseText);
+    var msg = json.Messages.join("<br/>");
+    if (json.Success) {
+        //remove item from comment list
+        $(".item a.delete[href$=" + json.Model + "]").parent(".item").remove();
+    } else {
+        alert(msg);
+    }
+}
+//#endregion
+//#region Contact Page
+function SubmitContact(response) {
+    var json = $.parseJSON(response.responseText);
+    if (!!json.Messages) {
+        var msg = json.Messages.join("<br/>");
+        alert(msg);
+    }
+    if (json.ReloadPage) {
+        window.location.reload();
+    } else {
+        document.getElementsByTagName("form")[0].reset();
+    }
+}
+//#endregion
