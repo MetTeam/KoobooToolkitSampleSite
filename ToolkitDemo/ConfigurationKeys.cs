704706d6498e8c679ed2d499db9d95cbf088764e
Tsingbo-Kooboo <qingbo@kooboo.com>
2013-11-29 11:06:43 +0800

    Sample Job Development

---
+++ ToolkitDemo/ConfigurationKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolkitDemo
+{
+    public class ConfigurationKeys
+    {
+        public const string SampleJobInterval = "SampleJobInterval";
+        public const string SampleJobEnabled = "SampleJobEnabled";
+        public const string SampleJobStarted = "SampleJobStarted";
+
+    }
+}
